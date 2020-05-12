333411aad34f34052debb6ac6bfaa59e31022383
Sabbir <sabbir106706@gmail.com>
2020-05-12 23:33:22 +0600

    p5 using cpp

---
+++ Sum OR Difference/Sum OR Difference .cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int a ,b;
+    cin>>a;
+    cin>>b;
+    if(a>b)
+    {
+        cout<<a-b;
+    }
+    else
+    {
+        cout<<a+b;
+    }
+    return 0;
+}
+
+
