2325de4d7d54d38debe1470c4456fac21fbc9a6c
Sabbir <sabbir106706@gmail.com>
2020-05-12 23:57:31 +0600

    p6 using cpp

---
+++ Cut the Board/Cut the Board.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int T;
+    cin>>T;
+    while(T--)
+    {
+        int N,M;
+        cin>>N>>M;
+        cout<<((N*M)-(N+M)+1)<<"\n";
+
+    }
+    return 0;
+}
+
+
