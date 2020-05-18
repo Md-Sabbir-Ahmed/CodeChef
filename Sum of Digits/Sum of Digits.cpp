a0b917011cd14993b693470c72ce0e2605c5ebca
Sabbir <sabbir106706@gmail.com>
2020-05-18 21:11:07 +0600

    p9 using cpp

---
+++ Sum of Digits/Sum of Digits.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    int t,n,temp,r,sum=0;
+    cin>>t;
+    while(t--)
+    {
+        cin>>n;
+        temp=n;
+        while(temp!=0)
+        {
+            r=temp%10;
+            sum=sum+r;
+            temp=temp/10;
+        }
+        cout<<sum<<"\n";
+        sum=0;
+    }
+
+    return 0;
+}
+
+
